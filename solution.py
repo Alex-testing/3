7994b333d8f4d6e3a17aea83fdeb70a47a933c9f
Alex-testing <60750525+Alex-testing@users.noreply.github.com>
2020-02-08 10:07:42 +0200

    First version

---
+++ solution.py
@@ -0,0 +1,9 @@
+# [('blood type', 'blood group')]
+recipients = [['B1', 'Rh+', 'John'], ['B1', 'Rh-', 'Julette'], ['B2', 'Rh+', 'Lestat'], ['B2', 'Rh-', 'Flocky'],
+              ['B3', 'Rh+', 'Creed'], ['B3', 'Rh-', 'Sam'], ['B4', 'Rh+', 'McLeod']]
+             #['B2', 'Rh+']]
+donors = [['B1', 'Rh+', 'Mary'], ['B3', 'Rh-', 'Dean'], ['B2', 'Rh-', 'Ragnar'], ['B3', 'Rh+', 'Logan'],
+          ['B1', 'Rh-', 'Romeo'], ['B2', 'Rh+', 'Louis'], ['B4', 'Rh-', 'Koschey']]
+
+non_ideal_couple = [[donor, recipient] for donor in donors for recipient in recipients if donor[0] == recipient[0]]
+ideal_couple = [couple for couple in non_ideal_couple if couple[0][1] == couple[1][1]]
